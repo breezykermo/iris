424be1e6c45817ca7cfc5510c4c73924b31bed2b
MithiJ <mithi_jethwa@brown.edu>
2024-10-11 12:40:22 -0400

    run script

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
+# Install docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+# Build and run docker image from docker/Dockerfile
 sudo docker build -t my-app docker/
 sudo docker run -it --name my-app-container my-app /bin/bash
