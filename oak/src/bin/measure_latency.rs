420e7fad69c14ce56e5f80e168290256394fe09f
MithiJ <mithi_jethwa@brown.edu>
2024-12-07 18:17:26 -0500

    recall calculations without gt

--- oak/src/bin/measure_latency.rs
+++ oak/src/bin/measure_latency.rs
@@ -7,6 +7,7 @@ use futures_util::{
     stream::{FuturesUnordered, Stream, StreamExt, TryStreamExt},
 };
 use slog_scope::{debug, info};
+use core::time;
 use std::time::Duration;
 use thiserror::Error;
 
@@ -54,98 +55,117 @@ impl Op {
     }
 }
 
+struct QueryStats {
+    query_op: Op,
+    recall: f64,
+    latency: Duration
+}
 /// time_req measures time taken for the vectorDB call
-async fn time_req(op: Op) -> Result<(Duration,), Report> {
+fn time_req(op: Op) -> Result<(Duration, Result<TopKSearchResultBatch, SearchableError>), Report> {
     let now = tokio::time::Instant::now();
-    let result = op.exec().await?;
+    let result = op.exec();
     Ok((now.elapsed(), result))
 }
+///Alt: compute gt for each query and log it into a CSV and then separately 
+/// run these to compare against pre-made gt
+/// Unsure if this is actually correct but keeping it around for reference
+// fn calculate_recall(search_result: &TopKSearchResultBatch, ground_truth: &GroundTruth) -> (f64, f64, f64) {
+//     let gt = calculate_gt(ground_truth);
+//     let nq = gt.len();
+//     let mut n_1 = 0;
+//     let mut n_10=0;
+//     let mut n_100 = 0;
 
-async fn req_loop(
-    mut ops: impl Stream<Item = (usize, Op)> + Unpin + Send + 'static,
-    done: tokio::sync::watch::Receiver<bool>,
-) -> Result<(Vec<Duration>, usize), Report> {
-    let mut durs = vec![];
-    let mut inflight = FuturesUnordered::new();
-    let mut arrv = std::time::Instant::now();
+//     for i in 0..nq {
+//         let gt_nn = gt[i*k]; // top 1 search
+//         for j in 0..k {
+//             if j <1 {
+//                 n_1 += 1;
+//             }
+//             if j < 10 {
+//                 n_10 +=1;
+//             }
+//             if j < 100 {
+//                 n_100 += 1;
+//             }
+//         }
+//     }
+//     (n_1 as f64 / nq as f64, 
+//         n_10 as f64 / nq as f64, 
+//         n_100 as f64 / nq as f64)
+// }
 
-    info!("starting requests");
-    loop {
-        // first check for a finished request.
-        let ops_val = match select(ops.next(), inflight.next()).await {
-            Either::Right((Some(Ok(d)), _)) => {
-                debug!(inflight = inflight.len(), "request done");
-                durs.push(d);
-                None
+
+async fn calculate_recall(gt: Vec<FlattenedVecs>, acorn_index: Vec<FlattenedVecs>, k: usize) {
+    // Some way of getting groundtruth -TODO: check FAISS for it
+    // Then decide the type of the input. Some Vec of Vec
+    nq = acorn_index.len();
+    gt = calculate_gt(gt);
+    let mut n_1 = 0;
+    let mut n_10= 0;
+    let mut n_100 = 0;
+    let mut rank = 1;
+    let mut results = vec![];
+    while rank <= k {
+        let gt_nn = &gt[0..rank]; // top-rank queries
+        let retrieved = &acorn_index[0..rank];
+        
+        // Compute intersection
+        let intersection = retrieved.iter().filter(|&&id| gt_nn.contains(&id)).count();
+        let recall = intersection as f64;
+        
+        recall /= rank as f64;
+        results.push(recall);
+        rank *= 10;
+    }
+    results // Recall for top-1, top-10 and top-100
+}
+
+/// This function is needed because the SIFT dataset doesn't have predicates. The
+/// predicates have been generated and uniformly distributed across a space. Now
+/// when we consult groundtruth indices, the top-k queries may not match the 
+/// required predicate and thus, shouldn't be counted in recall. Instead, we 
+/// take the top-k queries that match the predicate and compare against 
+/// OAK-generated results.
+fn calculate_gt(gt_index: Vec<FlattenedVecs>)-> Vec<FlattenedVecs> {
+    // TODO: Need Lachlan's help on loading in predicates and searching by them
+    // Is there something modular? Where are pedicates generated  (in case we
+    // distribute in range 1-30 or add more predicates to search by or sth)
+    vec![]
+}
+
+/// Loading the groundtruth is simply loading in the fvecs file. The complexity
+/// of checking predicate matches is handed over to the calculate recall 
+fn load_gt(path: &str) ->  {
+    // TODO: Parse CSV into a usable GroundTruth data structure
+    Ok(GroundTruth {})
+}
+
+/// This query loop essentially takes a query load, finds the latency of running
+/// the queries one at a time, looks at the total time taken and determines QPS
+/// and finally, logs the recall for each query as compared to the groundtruth
+/// indices
+fn query_loop (
+    ops: Vec<Op> // each Op knows 
+) -> Result<(), Report> {
+    let benchmark_results = vec![];
+    for op in ops {
+        match time_req(op) {
+            Ok((latency, Ok((res, err)))) => {
+                let recall = calculate_recall(gt, res);
+                benchmark_results.push(QueryStats(
+                    op,
+                    recall,
+                    latency
+                ))
             }
-            Either::Right((Some(Err(e)), _)) => return Err(e),
-            Either::Right((None, f)) => Some(f.await),
-            Either::Left((x, _)) => Some(x),
-        };
-
-        // if after the above, something happened in incoming request stream -- either the
-        // stream directly yielded, or inflight gave us None and we then waited for a request -- then handle that.
-        if let Some(ov) = ops_val {
-            match ov {
-                Some((remaining_cnt, o)) if remaining_cnt > 0 => {
-                    inflight.push(time_req(o));
-                    let interarrv = arrv.elapsed();
-                    arrv = std::time::Instant::now();
-                    debug!(
-                        remaining_cnt,
-                        inflight = inflight.len(),
-                        ?interarrv,
-                        "new request"
-                    );
-                }
-                _ => {
-                    info!(completed = durs.len(), "finished requests");
-                    break;
-                }
-            }
-        }
-
-        // This can't be inside the select because then the else clause would never be
-        // triggered.
-        if *done.borrow() {
-            debug!(completed = durs.len(), "stopping");
-            break; // the first client finished. stop.
+            Ok((_, Err(e))) => {println!("Search operation failed with error {:?}", e)}
+            Err(e) => {println!("Timing operation failed with error {:?}", e)}
         }
     }
-
-    Ok::<_, Report>((durs, inflight.len()))
+    Ok(())
 }
 
-/// Generates a stream of Ops to be passed to `req_loop` from a dataset, queries, and filter_id_map.
-/// This was taken from -
-/// https://github.com/akshayknarayan/burrito/blob/1649718d9acb96c440ee423cf43688ffbeed9a5d/kvstore-ycsb/src/lib.rs#L73
-/// - a benchmarking project. It was modified to fit vector operations instead of DB operations
-pub fn const_paced_ops_stream(
-    dataset: &FvecsDataset,
-    queries: Vec<&FlattenedVecs>,
-    filter_id_map: &Vec<c_char>,
-    k: usize,
-    interarrival_micros: u64,
-) -> impl Stream<Item = (usize, Op)> {
-    let len = queries.len();
-    let tkr = SpinTicker::new_const(Duration::from_micros(interarrival_micros))
-        .zip(futures_util::stream::iter((0..len).rev()));
-
-    tkr.zip(futures_util::stream::iter(queries))
-        .map(move |((_, i), query_vector)| {
-            // Each item in the stream will include:
-            // (Index, Operation, Filter Map, K-value))
-            (
-                i,
-                Op {
-                    dataset,
-                    query_vector,
-                    filter_id_map,
-                    k,
-                },
-            )
-        })
-}
 
 fn main() -> Result<()> {
     let log = ConfigLogging::StderrTerminal {
@@ -169,6 +189,11 @@ fn main() -> Result<()> {
 
     let _ = dataset.initialize(&opts);
     info!("Seed index constructed.");
+    // Load queries
+    // Load predicates
+    // Load GT
+    // pop from both, run queryloop 
+    // log latency, operation (query vec and predicate searched for) and recall
 
     // let dimensionality = dataset.get_dimensionality() as usize;
     // info!("Constructing random vector to query with {dimensionality} dimensions");
