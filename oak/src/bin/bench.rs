5b1dbc8d351e168a45ee2f3521a133060758055f
MithiJ <mithi_jethwa@brown.edu>
2024-12-13 01:06:32 -0500

    refactor

--- oak/src/bin/bench.rs
+++ oak/src/bin/bench.rs
@@ -24,12 +24,12 @@ pub enum ExampleError {
     ServerStartError(String),
 }
 
-// #[derive(Parser, Debug)]
-// #[command(version, about, long_about = None)]
-// struct Args {
-//     #[arg(short, long, required(true))]
-//     dataset: String,
-// }
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, required(true))]
+    dataset: String,
+}
 struct ExpResult {
     durs: Vec<Duration>,
     remaining_inflight: usize,
@@ -43,52 +43,6 @@ struct QueryStats {
     recall_100: bool,
     latency: Duration
 }
-/// time_req measures time taken for the vectorDB call
-// fn time_req(
-//     dataset: &FvecsDataset,
-//     query_vector: &FlattenedVecs,
-//     filter_id_map: &Bitmask,
-//     k: usize) -> Result<(Duration, Result<TopKSearchResultBatch, SearchableError>)> {
-//     let now = tokio::time::Instant::now();
-//     let result = dataset.search_with_bitmask(&query_vector, *filter_id_map, k);
-//     Ok((now.elapsed(), result))
-// }
-
-/// This query loop essentially takes a query load, finds the latency of running
-/// the queries one at a time, looks at the total time taken and determines QPS
-/// and finally, logs the recall for each query as compared to the groundtruth
-/// indices
-// fn query_loop (
-//     dataset: FvecsDataset,
-//     query_vectors: Vec<FlattenedVecs>, // TODOM: Ask Lachlan whether its Vec of or not
-//     filter_id_map: Bitmask,
-//     k: usize,
-//     gt: Vec<usize>
-// ) -> Result<Vec<QueryStats>> {
-//     let mut benchmark_results = vec![];
-//     for (index, (op, gt)) in query_vectors.iter().zip(gt.iter()).enumerate() {
-//         match time_req(&dataset, &op, &filter_id_map, k) {
-//             Ok((latency, Ok(res))) => {
-//                 let recall = calculate_recall_1(gt, res);
-//                 match recall {
-//                     Ok((r1, r10, r100)) => {
-//                         benchmark_results.push(QueryStats{
-//                             recall_1 : r1,
-//                             recall_10: r10,
-//                             recall_100: r100,
-//                             latency: latency
-//                         });
-//                     }
-//                     Err(_) => info!("Some error running recall calculation")
-//                 }
-//             }
-//             Ok((_, Err(e))) => {println!("Search operation failed with error {:?}", e)}
-//             Err(e) => {println!("Timing operation failed with error {:?}", e)}
-//         }
-//     }
-//     info!("Completed benchmarking queries!");
-//     Ok(benchmark_results)
-// }
 
 fn averages(queries: Vec<QueryStats>) -> Result<(f32, f32, f32, f32)> {
     let total_latencies:f32 = queries.iter().map(|qs| qs.latency).sum::<Duration>().as_secs() as f32;
@@ -99,32 +53,25 @@ fn averages(queries: Vec<QueryStats>) -> Result<(f32, f32, f32, f32)> {
     Ok((total_latencies as f32 /count as f32, total_r1 as f32 /count as f32, total_r10 as f32 /count as f32, total_r100 as f32 /count as f32))
 }
 
-fn calculate_recall_1(gt: &usize, acorn_result: TopKSearchResultBatch) -> Result<(bool, bool, bool)> {
-    // todo!(); // Ask lachlan how to iterate through TopKSearchResbatch
+fn calculate_recall_1(gt: &Vec<usize>, acorn_result: TopKSearchResultBatch) -> Result<(f32)> {
     // Figure out how to represent the Groundtruth and index into it!!
-    let mut n_1= false;
-    let mut n_10 = false;
-    let mut n_100 = false;
-    for (i, j) in acorn_result[0].iter().enumerate() {
-        if j.0 == *gt {
-            if i <1 {
-                n_1 = true;
-                n_10 = true;
-                n_100 = true;
-                break
-            }
-            if i < 10 {
-                n_10 = true;
-                n_100 = true;
-                break
-            }
-            if i < 100 {
-                n_100 = true;
-                break
+    let mut n_1: usize= false;
+    let mut n_10: usize = false;
+    for (query_index, neighbors) in acorn_result.iter().enumerate() {
+        let groundNN = gt[query_index];
+        for (i, j) in neighbors.iter().enumerate() {
+            if j.0 == groundNN {
+                if i <1 {
+                    n_1 += 1;
+                }
+                if i < 10 {
+                    n_10 += 1;
+                }
+                break;
             }
         }
     }
-    Ok((n_1, n_10, n_100))
+    Ok(n_10 as f32/ gt.len() as f32)
 }
 
 fn read_csv(file_path: &str) -> Result<Vec<usize>> {
@@ -136,7 +83,6 @@ fn read_csv(file_path: &str) -> Result<Vec<usize>> {
         let value: usize = record[0].parse::<usize>()?; // why is this needed?
         values.push(value);
     }
-
     Ok(values)
 }
 
@@ -149,64 +95,57 @@ fn main() -> Result<()> {
 
     let _guard = slog_scope::set_global_logger(log.clone());
 
-    // let args = Args::parse();
+    let args = Args::parse();
 
-    // let mut dataset = FvecsDataset::new(args.dataset)?;
-    // info!("Dataset loaded from disk.");
+    let mut dataset = FvecsDataset::new(args.dataset)?;
+    info!("Dataset loaded from disk.");
 
-    // let opts = OakIndexOptions {
-    //     gamma: 1,
-    //     m: 32,
-    //     m_beta: 64,
-    // };
-    // // How are they learning about dataset predicates?
+    let opts = OakIndexOptions {
+        gamma: 1,
+        m: 32,
+        m_beta: 64,
+    };
+    // How are they learning about dataset predicates?
 
-    // let _ = dataset.build_index(&opts);
-    // info!("Seed index constructed.");
+    let _ = dataset.build_index(&opts);
+    info!("Seed index constructed.");
 
-    // let query = PredicateQuery::new(5);
+    let query = PredicateQuery::new(1);
 
-    // let mut subdataset = dataset.view(&query);
-    // let _ = subdataset.build_index(&opts);
-    // info!("Subindex as view constructed.");
+    let mut subdataset = dataset.view(&query);
+    let _ = subdataset.build_index(&opts);
+    info!("Subindex as view constructed.");
 
-    // let dimensionality = dataset.get_dimensionality() as usize;
-    // assert_eq!(dimensionality, subdataset.get_dimensionality() as usize);
+    let dimensionality = dataset.get_dimensionality() as usize;
+    assert_eq!(dimensionality, subdataset.get_dimensionality() as usize);
 
-    // let mut dataset = FvecsDataset::new(args.query)?;
-    // info!("Query set loaded from disk.");
+    let mut dataset = FvecsDataset::new(args.query)?;
+    info!("Query set loaded from disk.");
 
-    // let topk = 10;
-    // let num_queries = query_vector.len();
+    let topk = 10;
+    let num_queries = query_vector.len();
 
-    // let mask_main = Bitmask::new(&query, &dataset);
-    // let mask_sub = Bitmask::new_full(&subdataset);
+    let mask_main = Bitmask::new(&query, &dataset);
+    let mask_sub = Bitmask::new_full(&subdataset);
 
-    // info!("Searching full dataset for {topk} similar vectors for {num_queries} random query , where attr is equal to 5...");
+    info!("Searching full dataset for {topk} similar vectors for {num_queries} random query , where attr is equal to 5...");
 
-    // let now = tokio::time::Instant::now();
-    // let result = dataset.search_with_bitmask(&query_vector, mask_main, topk);
-    // let end = now.elapsed();
+    let now = tokio::time::Instant::now();
+    let result = dataset.search_with_bitmask(&query_vector, mask_main, topk);
+    let end = now.elapsed();
 
-    // let latency = end / num_queries;
+    let latency = end / num_queries;
+    info!("QPS is {latency}");
 
     info!("GT loading...");
     let groundtruth_path = "data/outdir/sift_groundtruth.csv";
     // let variable_gt_path = "./outdir/sift_groundtruth.csv";
     let gt = read_csv(groundtruth_path);
 
-    
+    let recall = calculate_recall_1(gt, result);
 
-    // Load predicates
-    // let predicate = Some(PredicateQuery::new(1));
-    // let filter_id_map = todo!(); // TODOM: ask Lachlan!
-    // let latencies = vec![];
-    // // TODO: vary efSearch
-    // let res = query_loop(dataset, queries, filter_id_map, k);
-    // match res {
-    //     Ok((qs)) => {latencies.append(averages(qs))}
-    //     Err(_) => {println!("Some error in calculating top-k vectors")}
-    // }
+    info!("Recall@10 is {recall}");
+
 
     // Log results to CSV
     // let mut wtr = Writer::from_path("output.csv")?;
